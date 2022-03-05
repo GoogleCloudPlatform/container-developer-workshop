8df6baace8d7adf2dac39328e260d4064d0c62eb
Dan Dobrin <dan@dans-mbp.lan>
2022-03-05 09:56:32 -0500

    First commit - base  Spring Boot app

---
+++ labs/spring-boot/demo-app/src/main/java/com/example/HelloController.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+
+@RestController
+public class HelloController {
+
+    @Value("${target:local}")
+    String target;
+
+    @GetMapping("/hello") 
+    public String hello()
+    {
+        return String.format("Hello from your %s environment!", target);
+    }
+}
---
+++ sample-apps/java/spring-boot/src/main/java/com/example/HelloController.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+
+@RestController
+public class HelloController {
+
+    @Value("${target:local}")
+    String target;
+
+    @GetMapping("/hello") 
+    public String hello()
+    {
+        return String.format("Hello from your %s environment!", target);
+    }
+}
