9ec47c2327fa2df4447c22155a43f6b5b3b023a8
ddobrin <ddobrin@google.com>
2022-03-07 13:35:47 -0500

    dependency cleanup

--- sample-apps/java/spring-boot/src/main/java/com/example/HelloController.java
+++ sample-apps/java/spring-boot/src/main/java/com/example/HelloController.java
@@ -12,7 +12,7 @@ public class HelloController {
     @Value("${target:local}")
     String target;
 
-    @GetMapping("/hello") 
+    @GetMapping("/") 
     public String hello()
     {
         return String.format("Hello from your %s environment!", target);
