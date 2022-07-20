94376e46651d61ff6957608dff37e7f952545816
VeerMuchandi <veer.muchandi@gmail.com>
2022-07-20 15:41:51 -0400

    Added USE_GKE_GCLOUD_AUTH_PLUGIN=True (#9)

--- labs/spring-boot/setup.sh
+++ labs/spring-boot/setup.sh
@@ -3,6 +3,7 @@
 ## Setup Variables
 export PROJECT_ID=$(gcloud config get-value project)
 export PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format='value(projectNumber)')
+export USE_GKE_GCLOUD_AUTH_PLUGIN=True
 
 
 export DB_INSTANCE_NAME=quote-db-instance
