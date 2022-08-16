77c536a6d244b91153b5e121ddc4180dd5aeaf61
VeerMuchandi <veer.muchandi@gmail.com>
2022-08-16 09:59:30 -0400

    added USE_GKE_GCLOUD_AUTH_PLUGIN=True (#13)
    to prevent Gcloud auth warnings

--- labs/python/setup.sh
+++ labs/python/setup.sh
@@ -2,6 +2,8 @@
 export PROJECT_ID=$(gcloud config get-value project)
 export PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format='value(projectNumber)')
 
+export USE_GKE_GCLOUD_AUTH_PLUGIN=True
+
 gcloud services enable \
     cloudresourcemanager.googleapis.com \
     container.googleapis.com \
