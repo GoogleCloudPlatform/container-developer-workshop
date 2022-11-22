9c7db55ec51619960683ee03c8523f6afe92a40b
VeerMuchandi <veer.muchandi@gmail.com>
2022-11-22 00:13:49 -0500

    added workstation config setup

---
+++ labs/python/workstation_config_setup.sh
@@ -0,0 +1,39 @@
+mkdir cw
+
+export REGION=us-central1
+export PROJECT_ID=$(gcloud config get-value project)
+export PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format='value(projectNumber)')
+export WS_CLUSTER=my-cluster
+export IMAGE=us-central1-docker.pkg.dev/cloud-workstations-images/predefined/code-oss:latest
+export CONFIG=codeoss-config.json
+export NAME=codeoss
+
+cat <<EOF > cw/$CONFIG
+{
+  "idleTimeout": "7200s",
+  "host": {
+    "gce_instance": {
+      "machine_type": "e2-standard-8",
+      "pool_size": 1,
+      "service_account": "$PROJECT_NUMBER-compute@developer.gserviceaccount.com",
+    },
+  },
+  "persistentDirectories": {
+    "mountPath": "/home",
+    "gcePd": {
+      "sizeGb": 200,
+      "fsType": "ext4"
+    }
+  },
+  "container": {
+          "image" : "$IMAGE"
+  }
+}
+EOF
+
+curl -H "Authorization: Bearer $(gcloud auth print-access-token)" \
+     -H "Content-Type: application/json" \
+     -d @cw/${CONFIG} \
+https://workstations.googleapis.com/v1beta/projects/${PROJECT_ID}/locations/$REGION/workstationClusters/${WS_CLUSTER}/workstationConfigs?workstation_config_id=${NAME}
+
+rm -rf cw
